a6b41de14801df5f4b8cbc17ff0b9fbbbd07d669
Gurpreet Singh <gurpreet.singh@preqin.com>
2019-07-09 17:51:14 +0100

    user features

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import { User } from './model/User';
 
-const user = new User({ name: 'xon' });
+const user = new User({ id: 1, name: 'better ebver' });
+
+user.on('change', () => {
+  console.log(user);
+});
+
+user.fetch();
 
 user.save();
